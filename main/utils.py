b75a317112e0c3d1ccf9e6571de8a0cfb63357d2
Quentin Loridant <q.loridant@gmail.com>
2024-10-28 14:33:45 +0100

    Remplacer shop id par nom + Footer

--- main/utils.py
+++ main/utils.py
@@ -1,9 +1,36 @@
+import requests
+
+def get_open_prices(session, shop_id):
+    url = f"https://prices.openfoodfacts.org/api/v1/prices?location_id={shop_id}&size=100&order_by=-date"
+    first_page = session.get(url).json()
+    yield first_page
+    num_pages = first_page['pages']
+
+    for page in range(2, num_pages + 1):
+        next_page = session.get(url, params={'page': page}).json()
+        yield next_page
+
 def keep_products_intersection_shops(shops, shop_products):
     common_shop_products = {}
     for barcode, product in shop_products.items():
-        if barcode == '3023480110707':
-            print('chartreuse')
         prices = product['prices']
         if prices.get(shops[0]["id"]) and prices.get(shops[1]["id"]):
+            if prices.get(shops[0]["id"]) < prices.get(shops[1]["id"]):
+                shop_products[barcode]['cheapest'] = shops[0]["id"]
+            else:
+                shop_products[barcode]['cheapest'] = shops[1]["id"]
             common_shop_products[barcode] = shop_products[barcode]
     return common_shop_products
+
+def summarize_shop_products(shops, shop_products):
+    summary = {}
+    summary['len'] = len(shop_products)
+    summary['prices'] = {}
+    for shop in shops:
+        summary['prices'][shop['id']] = {}
+        summary['prices'][shop['id']]['sum_prices'] = 0
+        for products in shop_products.values():
+            summary['prices'][shop['id']]['sum_prices'] += products['prices'][shop['id']]
+        
+        summary['prices'][shop['id']]['sum_prices'] = "{:.2f}".format(summary['prices'][shop['id']]['sum_prices'])
+    return summary
--- main/views.py
+++ main/views.py
@@ -1,27 +1,28 @@
 import requests
 from django.shortcuts import render
 import xmltodict
-from .utils import keep_products_intersection_shops
+from .utils import keep_products_intersection_shops, summarize_shop_products, get_open_prices
 
 def shop_compare_summary(request):
     # Get user's selected location or shop type, then call OSM API
     selected_shops = []
     
-    shop_id_1 = request.GET.get('osm_id_shop_1') if request.GET.get('osm_id_shop_1') else '27108404' # Get shop ID from query parameter Intermarché
-    shop_id_2 = request.GET.get('osm_id_shop_2') if request.GET.get('osm_id_shop_2') else '872934393' # Get shop ID from query parameter Auchan
+    # shop_id_1 = request.GET.get('osm_id_shop_1') if request.GET.get('osm_id_shop_1') else '27108404' # Get shop ID from query parameter Intermarché
+    shop_id_1 = request.GET.get('osm_id_shop_1') if request.GET.get('osm_id_shop_1') else '154' # Get shop ID from query parameter Intermarché
+    shop_id_2 = request.GET.get('osm_id_shop_2') if request.GET.get('osm_id_shop_2') else '3' # Get shop ID from query parameter Auchan
         
     # Fetch shops from OpenStreetMap
     for shop_id in [shop_id_1, shop_id_2]:
         shop = {'id': shop_id}
-        response = requests.get(f"https://www.openstreetmap.org/api/0.6/node/{shop_id}")
-        data = xmltodict.parse(response.content)['osm']['node'] if response.status_code == 200 else {}
-        if 'tag' in data.keys():
-            for node in data['tag']:
-                if node['@k'] == 'name':
-                    shop['name'] = node['@v']
-                if node['@k'] == 'addr:city':
-                    shop['city'] = node['@v']
-                pass
+        # response = requests.get(f"https://www.openstreetmap.org/api/0.6/node/{shop_id}")
+        # data = xmltodict.parse(response.content)['osm']['node'] if response.status_code == 200 else {}
+        # if 'tag' in data.keys():
+        #     for node in data['tag']:
+        #         if node['@k'] == 'name':
+        #             shop['name'] = node['@v']
+        #         if node['@k'] == 'addr:city':
+        #             shop['city'] = node['@v']
+        #         pass
         selected_shops.append(shop)
     
     # Receive list of selected shops and retrieve product prices from OFF
@@ -29,22 +30,28 @@ def shop_compare_summary(request):
     shop_products = {}
     products = {}
     for shop in selected_shops:
-        prices = requests.get(f"https://prices.openfoodfacts.org/api/v1/prices?location_osm_id={shop['id']}").json()
-        for price in  prices['items']:            
-            product = price.get('product')
-            if not product:
-                continue
-            
-            if product.get('code') not in products.keys():  ## Init the dict if it s the first time
-                products[product.get('code')] = {}
-                products[product.get('code')]['name'] = product.get('product_name')
-                products[product.get('code')]['prices'] = {}
+        session = requests.Session()
+        for prices in get_open_prices(session, shop['id']): 
+            for price in prices['items']:
+                if not shop.get('name'):
+                    location = price.get('location')
+                    shop['name'] = location['osm_name']
+                
+                product = price.get('product')
+                if not product:
+                    continue
+                
+                if product.get('code') not in products.keys():  ## Init the dict if it s the first time
+                    products[product.get('code')] = {}
+                    products[product.get('code')]['name'] = product.get('product_name')
+                    products[product.get('code')]['prices'] = {}
 
-            products[product.get('code')]['prices'][shop["id"]] = price['price']
+                if not products[product.get('code')]['prices'].get(shop["id"]): # Do not overide prices to get the latest ones
+                    products[product.get('code')]['prices'][shop["id"]] = price['price']
 
     
     shop_products = keep_products_intersection_shops(selected_shops, products)
-        
-    return render(request, 'main/shop_summary.html', {'shops': selected_shops, 'products': shop_products})
+    shop_products_summary = summarize_shop_products(selected_shops, shop_products)
+    return render(request, 'main/shop_summary.html', {'shops': selected_shops, 'products': shop_products, 'shop_products_summary': shop_products_summary})
 
 
