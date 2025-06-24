150d0b73d80486177b1ff03f8609477426eacbda
qloridant <quentin.loridant@protonmail.com>
2025-06-24 12:11:43 +0200

    Update settings.py

--- open_shop_compare/settings.py
+++ open_shop_compare/settings.py
@@ -89,7 +89,10 @@ WSGI_APPLICATION = 'open_shop_compare.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
