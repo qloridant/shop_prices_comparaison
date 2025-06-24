aa83eeb60d78588d2dfb531d54ac0ee6bc6f949b
qloridant <quentin.loridant@protonmail.com>
2025-06-24 12:24:48 +0200

    Ajout post compile in start script

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Post compile
+export PYTHONPATH=/build/${REQUEST_ID}/.apt/usr/lib/python3/dist-packages/:${PYTHONPATH}
+python manage.py collectstatic --noinput
+
+# Post deploy
 python manage.py migrate
+
+# Start
 gunicorn open_shop_compare.wsgi --log-file -
 
 
